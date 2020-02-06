3e29015f3aecc91d0813b8e33d05003f83e42f76
Ubuntu <ubuntu@ip-172-31-88-141.ec2.internal>
2020-02-06 19:51:24 +0000

    dont want these changes

--- check_vid_django/check_vid_src/Report/views.py
+++ check_vid_django/check_vid_src/Report/views.py
@@ -13,8 +13,10 @@ from time import time
 from gensim.models import Word2Vec, KeyedVectors
 from gensim.test.utils import get_tmpfile
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
+
 lemma=nltk.stem.WordNetLemmatizer()#from nltk.stem import WordNetLemmatizer as lemma
 
 
